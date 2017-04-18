f3e1b183d20fa136090f9f08a7ab3518c92df89c
grrrben <grrrben@gmail.com>
2017-04-18 21:13:32 +0200

    added keypress file which was lost on my very old laptop

---
+++ keypress.h
@@ -0,0 +1,97 @@
+void editorProcessKeypress() {
+    static int quit_times = EDITOR_QUIT_TIMES;
+    int c = editorReadKey();
+
+    /*
+    if (iscntrl(c)) {
+        editorSetStatusMessage("%d\n", c);
+    } else {
+        editorSetStatusMessage("%d ('%c')\n", c, c);
+    }
+    */
+
+    switch (c) {
+        case '\r':
+            /* Enter key */
+            editorInsertNewline();
+            break;
+
+        case CTRL_KEY('q'):
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage(
+                        MESSAGE_QUIT_WARNING,
+                        quit_times
+                );
+                quit_times--;
+                return;
+            }
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+
+        case CTRL_KEY('s'):
+            editorSave();
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            if (E.cy < E.numrows)
+                E.cx = E.row[E.cy].size;
+            break;
+
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
+
+        case CTRL_KEY('a'):
+            /* todo select all*/
+            break;
+
+        case BACKSPACE:
+        case DEL_KEY:
+            if (c == DEL_KEY) {
+                editorMoveCursor(ARROW_RIGHT);
+            }
+            editorDelChar();
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            if (c == PAGE_UP) {
+                E.cy = E.rowoff;
+            } else {
+                E.cy = E.rowoff + E.screenrows - 1;
+                if (E.cy > E.numrows) E.cy = E.numrows;
+            }
+
+            int times = E.screenrows;
+            while (times--) {
+                /* Move up or down with the arrow keys, just repeat it *times times */
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+        }
+            break;
+
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+
+        case CTRL_KEY('l'):
+        case '\x1b':
+            break;
+
+        default:
+            /* not a control key */
+            editorInsertChar(c);
+            break;
+    }
+
+    quit_times = EDITOR_QUIT_TIMES;
+}
